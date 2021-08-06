be5959b28eeed4fd7cbb15cf32b6e5deff2840cc
Cleeyv <cleeyv@riseup.net>
2021-08-06 12:18:50 -0400

    Add user to the build server

--- configuration.nix
+++ configuration.nix
@@ -183,6 +183,13 @@
     ];
   };
 
+  users.users.cleeyv = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO4p4CqilI3n1GOyGcDgUh1UpwxeHSTIiV4oeHYjF431 cleeyv"
+    ];
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you

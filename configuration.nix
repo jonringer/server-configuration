24f58770b763e7c4629f497cf9cb53b4b79266a2
Daniel KT <dan@distrosync.com>
2021-08-09 11:51:28 -0400

    add tshaynik as normal user

--- configuration.nix
+++ configuration.nix
@@ -204,6 +204,13 @@
     ];
   };
 
+  users.users.tshaynik = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3wOOYiW+G1u5zHpvzq0N+nhj5l9o8lMht4kYias28n tshaynik"
+    ];
+  };
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you

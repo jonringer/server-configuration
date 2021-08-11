4e9f2acc7db2eb1a25a12b55ba001b037f9476e3
tshaynik <dkundathagard@gmail.com>
2021-08-11 11:26:37 -0400

    add tshaynik as normal user (#6)
    Co-authored-by: Jonathan Ringer <jonringer@users.noreply.github.com>

--- configuration.nix
+++ configuration.nix
@@ -204,6 +204,13 @@
     ];
   };
 
+  users.users.tshaynik = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3wOOYiW+G1u5zHpvzq0N+nhj5l9o8lMht4kYias28n tshaynik"
+    ];
+  };
+
   users.users.artturin = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [

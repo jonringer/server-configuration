67f1732282fd1953c59ca1589af1070159b5e533
Milan Hauth <milahu@gmail.com>
2022-11-21 08:01:57 +0100

    Revert "nix: 2.7 -> unstable-2022-11-16"
    This reverts commit 332e496f5ff07f074445879e3c9a8161c170d7a9.
    
    nix-daemon requires nix >= 2.2
    
    lib.versionOlder "2.2" "unstable-2022-11-16" == false
    lib.versionOlder "2.2" "2.12.0" == true

--- flake.nix
+++ flake.nix
@@ -3,10 +3,7 @@
   #inputs.nixpkgs.url = "path:/home/jon/projects/nixpkgs";
   inputs.hydra.url = "github:NixOS/hydra";
 
-  # https://github.com/NixOS/nix/pull/7283
-  inputs.nixSource.url = "github:NixOS/nix/62960f32915909a5104f2ca3a32b25fb3cfd34c7";
-
-  outputs = { self, nixpkgs, hydra, nixSource }: {
+  outputs = { self, nixpkgs, hydra }: {
 
     nixosConfigurations.server = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
@@ -15,7 +12,6 @@
       ];
       specialArgs = {
         inherit (hydra.packages.${system}) hydra;
-        inherit nixSource;
       };
     };
 

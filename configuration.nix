ebb0791a9c34e1456ba41fc4eff7bff03718c319
Artturin <Artturin@artturin.com>
2023-08-24 18:14:40 +0300

    improve system responsiveness
    nixpkgs PR 250308

--- configuration.nix
+++ configuration.nix
@@ -65,6 +65,12 @@
   boot.supportedFilesystems = [ "zfs" ];
   boot.zfs.forceImportAll = true;
   services.zfs.trim.enable = true;
+
+  # ZFS already has its own scheduler. Without this my(@Artturin) computer froze for a second when I nix build something.
+  services.udev.extraRules = ''
+    ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+  '';
+
   hardware.cpu.amd.updateMicrocode = true;
   boot.extraModprobeConfig = ''
     options kvm-amd nested=1

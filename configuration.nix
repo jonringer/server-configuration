8e5b26e0e1512a2bccb6f0390aabd2c799bbfc8c
Jonathan Ringer <jonringer117@gmail.com>
2024-05-05 12:19:32 -0700

    Remove individuals who expressed potential ill-will for me

--- configuration.nix
+++ configuration.nix
@@ -435,23 +435,6 @@
         ];
       };
 
-      # @yayayayaka
-      yaya = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+8UDzJpCVXn/KmkjtkBqoBhREIqnm+4uU1gPFwwuTl irisu"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHS7O+fAO1XscN625WUWZOQVV6wbjXoGajXlBqV5kB1 krul"
-        ];
-      };
-
-      # @yu-re-ka
-      yuka = {
-        isNormalUser = true;
-        openssh.authorizedKeys.keys = [
-          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCzSwdjZ3t0SKet6u5Mjz0lcNFC9ZUkkfNj401kwAPx9sXK6t30tPnYt+agVfyU290JWw+bnB4Rt1MQEMWTk/oQHFlkbBHW8JH6oJc6EOKV30AIKyJXCGDWQ3u86dN6GF0EIJf7WMDtlgKceY+NZprKkaXrmSUrreMiQc9fpEIWZa5+jPRLveHE1Z2yHEtf1zDNM1j0v1e3bje8xL50arDa0ZjSz00Skh3qJCMH8aAW1McgAGWa8FfwytTuByvpKh0Czk9Za3eAHTVDGVrNwBu0lDFEqYdxnv7lZvJiTrkG+9IFHUsbngQYEvOD8LW3WN/LKE3ErxhHSxRfPm/VIjcTZO3FEA2D0EdbXhyqTuhhysOg13ssrcjXfwWjIY+zdfBOgmaGttcd8gtl20YuXPbqbdqzEDZsDUFFre8Ef6w9ZTT98vabXPGsB7nWU5P3qfgU6dh5SE17AoT9/PnkcPUftHVtov5ToyNsRGclLcyCvfs+rA1AsbLjW7wpQ191ZDoGDT8bMDGheCaHGOlNFnCSZkbB5S8o6gynMJfiviCWR4MiH47oFD/n2z0i+5AafMWv5ZUWr1A0HycP8k6hr7Zc+JPMK9kun9hopVaWHA6lLMg92VDG1eeNCxK7Dv8v23PA5gTugbQvyfcwSECj3hPpzCEsCnyvFoF3BZkvDpJGeQ== yuka"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKath4/fDnlv/4fzxkPrQN1ttmoPRNu/m9bEtdPJBDfY cardno:16 933 242"
-        ];
-      };
     };
   };
   # }}}

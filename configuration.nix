c01083f50f6836f87a7afd7ea9a0d13f13fb78b3
Jonathan Ringer <jonringer117@gmail.com>
2026-09-04 06:54:46 -0700

    nix.gc: only collect paths older than 2 days

--- configuration.nix
+++ configuration.nix
@@ -272,6 +272,7 @@
       automatic = true;
       # every 3rd day
       dates = "*-*-1,4,7,10,13,16,19,22,25,28,31 00:00:00";
+      options = "--delete-older-than 2d";
     };
 
     nixPath = ["nixpkgs=/etc/nix/inputs/nixpkgs"];
